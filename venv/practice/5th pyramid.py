d8930cf654d3a3a1aade6ad2c30ce9f0036bdf91
jsethy2010 <jsethy2010@gmail.com>
2019-10-23 15:03:04 +0530

    sp

--- venv/practice/5th pyramid.py
+++ venv/practice/5th pyramid.py
@@ -1,5 +1,5 @@
 print("Fifth Number Pattern")
-lastNumber = 9
+lastNumber = int(input("enter a number for pattern: "))
 for i in range(1, lastNumber):
     for i in range(0, i, 1):
         print(format(2**i, "4d"), end=' ')
