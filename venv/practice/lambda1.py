01d30da3643e4edab8a65a63c753ae67c54c31a6
jsethy2010 <jsethy2010@gmail.com>
2019-10-23 14:58:16 +0530

    sp

--- venv/practice/lambda1.py
+++ venv/practice/lambda1.py
@@ -4,9 +4,9 @@ nums = int(input("enter a number to check odd/even: "))
 
 
 even = lambda nums : nums%2==0
-result = int(even(nums))
+result = even(nums)
 print(result)
 
-odd = lambda nums : nums%2!=0
-result1 = int(odd(nums))
-print(result1)
+#odd = lambda nums : nums%2!=0
+#result1 = int(odd(nums))
+#print(result1)
